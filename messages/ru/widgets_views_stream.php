3c6e530c7937c70a8e405979c238111d5b9ede01
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-13 10:36:25 +0200

    Updated translations (translate.humhub.org)

--- messages/ru/widgets_views_stream.php
+++ messages/ru/widgets_views_stream.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  '<b>There are no tasks yet!</b>' => '',
-  'Back to stream' => '@@Вернуться в поток@@',
-  'No tasks found which matches your current filter(s)!' => '@@Нет задач, соответствующих вашему фильтру!@@',
+  '<b>There are no tasks yet!</b>' => '<b>Здесь нет пока задач!</b>',
   '<b>There are no tasks yet!</b><br>Be the first and create one...' => '<b>Задач пока нет!</b><br>Будьте первым в создании...',
   'Assigned to me' => 'Назначенное для меня',
+  'Back to stream' => 'Вернуться в поток',
   'Created by me' => 'Создано мной',
   'Creation time' => 'Время создания',
   'Filter' => 'Фильтр',
   'Last update' => 'Последнее обновление',
+  'No tasks found which matches your current filter(s)!' => 'Нет задач, соответствующих вашему фильтру!',
   'Nobody assigned' => 'Никто не назначен',
   'Sorting' => 'Сортировка',
   'State is finished' => 'Статус завершено',
