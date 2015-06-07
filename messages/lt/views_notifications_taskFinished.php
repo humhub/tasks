995214a790b36c34bbf860f566b6f9498bc8dc3c
Lucas Bartholemy <lucas@bartholemy.com>
2015-06-07 21:25:04 +0200

    Added Lithuanian language files

---
+++ messages/lt/views_notifications_taskFinished.php
@@ -0,0 +1,4 @@
+<?php
+return array (
+  '{userName} finished task {task}.' => '',
+);
---
+++ messages/lt/widgets_views_entry.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'Click, to finish this task' => '',
+  'This task is already done' => '',
+  'This task is already done. Click to reopen.' => '',
+  'You\'re not assigned to this task' => '',
+);
