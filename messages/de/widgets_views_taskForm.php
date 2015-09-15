bfa262702dd0fe5f5b2f32ec2dc50a3536753f99
HumHub Translate <info@humhub.com>
2015-09-15 23:59:56 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/widgets_views_taskForm.php
+++ messages/de/widgets_views_taskForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Assign users to this task' => 'User zu dieser Aufgabe zuordnen',
-  'Deadline for this task?' => '',
+  'Deadline for this task?' => 'Deadline für diese Aufgabe?',
   'Preassign user(s) for this task.' => '',
   'What to do?' => 'Was ist zu tun?',
 );
