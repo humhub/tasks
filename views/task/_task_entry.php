59dc26c4ec6ecefe026a163aa6cc839bbd767fc7
Tobias Johannes <tobias.johannes@dmk-ebusiness.de>
2015-10-26 09:31:22 +0100

    view optimized of a single task

--- views/task/_task_entry.php
+++ views/task/_task_entry.php
@@ -55,58 +55,64 @@ foreach ( $task->assignedUsers as $au ) {
 	</div>
 
 	<div class="media-body">
-		<span class="pull-left">
-			<a href="<?php echo $task->creator->getUrl(); ?>" id="user_<?php echo $task->id; ?>"> 
-				<img src="<?php echo $task->creator->getProfileImage()->getUrl(); ?>" class="img-rounded tt" height="24" width="24" alt="24x24" style="width: 24px; height: 24px;" data-toggle="tooltip"
-					data-placement="top" title="" data-original-title="<?php echo Html::encode($task->creator->displayName); ?>">
-			</a>
-		</span>
-		<span class="task-title pull-left"><?php echo $task->title; ?></span>
-		<?php if ($task->hasDeadline()) : ?>
-           	<?php
-				$timestamp = strtotime ( $task->deadline );
-				$class = "label label-default";
-				if (date ( "d.m.yy", $timestamp ) <= date ( "d.m.yy", time () )) {
-					$class = "label label-danger";
-				}
-			?>
-        	<span class="<?php echo $class; ?>"><?php echo date("d. M", $timestamp); ?></span>
-		<?php endif; ?>
-		<div class="task-controls end pull-right">
-			<a href="<?php echo $contentContainer->createUrl('edit', ['id' => $task->id]); ?>" class="tt" data-target="#globalModal" data-toggle="tooltip" data-placement="top" 
-				data-original-title="Edit Task">
-				<i class="fa fa-pencil"></i>
-			</a>
-			<?php echo humhub\widgets\ModalConfirm::widget ( array (
-						'uniqueID' => 'modal_delete_task_' . $task->id,
-						'linkOutput' => 'a',
-						'title' => Yii::t ( 'TasksModule.views_task_show', '<strong>Confirm</strong> deleting' ),
-						'message' => Yii::t ( 'TasksModule.views_task_show', 'Do you really want to delete this task?' ),
-						'buttonTrue' => Yii::t ( 'TasksModule.views_task_show', 'Delete' ),
-						'buttonFalse' => Yii::t ( 'TasksModule.views_task_show', 'Cancel' ),
-						'linkContent' => '<i class="fa fa-times-circle-o colorDanger"></i>',
-						'linkHref' => $contentContainer->createUrl ( 'delete', array (
-							'id' => $task->id 
-						) ),
-						'confirmJS' => "$('#task_" . $task->id . "').fadeOut('fast')" 
-					) ); ?>
+		<div class="row">
+			<div class="col-sm-6">
+				<span class="pull-left">
+					<a href="<?php echo $task->creator->getUrl(); ?>" id="user_<?php echo $task->id; ?>"> 
+						<img src="<?php echo $task->creator->getProfileImage()->getUrl(); ?>" class="img-rounded tt" height="24" width="24" alt="24x24" style="width: 24px; height: 24px;" data-toggle="tooltip"
+							data-placement="top" title="" data-original-title="<?php echo Html::encode($task->creator->displayName); ?>">
+					</a>
+				</span>
+				<?php if ($task->hasDeadline()) : ?>
+		           	<?php
+						$timestamp = strtotime ( $task->deadline );
+						$class = "label label-default";
+						if (date ( "d.m.yy", $timestamp ) <= date ( "d.m.yy", time () )) {
+							$class = "label label-danger";
+						}
+					?>
+		        	<span class="<?php echo $class; ?>"><?php echo date("d. M", $timestamp); ?></span>
+				<?php endif; ?>
+			</div>
+			<div class="col-sm-6">
+				<div class="task-controls end pull-right">
+					<a href="<?php echo $contentContainer->createUrl('edit', ['id' => $task->id]); ?>" class="tt" data-target="#globalModal" data-toggle="tooltip" data-placement="top" 
+						data-original-title="Edit Task">
+						<i class="fa fa-pencil"></i>
+					</a>
+					<?php echo humhub\widgets\ModalConfirm::widget ( array (
+								'uniqueID' => 'modal_delete_task_' . $task->id,
+								'linkOutput' => 'a',
+								'title' => Yii::t ( 'TasksModule.views_task_show', '<strong>Confirm</strong> deleting' ),
+								'message' => Yii::t ( 'TasksModule.views_task_show', 'Do you really want to delete this task?' ),
+								'buttonTrue' => Yii::t ( 'TasksModule.views_task_show', 'Delete' ),
+								'buttonFalse' => Yii::t ( 'TasksModule.views_task_show', 'Cancel' ),
+								'linkContent' => '<i class="fa fa-times-circle-o colorDanger"></i>',
+								'linkHref' => $contentContainer->createUrl ( 'delete', array (
+									'id' => $task->id 
+								) ),
+								'confirmJS' => "$('#task_" . $task->id . "').fadeOut('fast')" 
+							) ); ?>
+				</div>
+				<div class="task-controls pull-right">
+					<a data-toggle="collapse" href="#task-comment-<?php echo $task->id; ?>" onclick="$('#comment_humhubmodulestasksmodelsTask_<?php echo $task->id; ?>').show();return false;"
+						aria-expanded="false" aria-controls="collapseTaskComments"><i class="fa fa-comment-o">
+						</i> <?php echo $count = Comment::GetCommentCount($task->className(), $task->id); ?>
+					</a>
+				</div>
+				<div class="task-controls assigned-users pull-right" style="display: inline;">
+					<!-- Show assigned user -->
+					<?php foreach ($task->assignedUsers as $user): ?>
+						<a href="<?php echo $user->getUrl(); ?>" id="user_<?php echo $task->id; ?>"> 
+							<img src="<?php echo $user->getProfileImage()->getUrl(); ?>" class="img-rounded tt" height="24" width="24" alt="24x24" style="width: 24px; height: 24px;" data-toggle="tooltip"
+								data-placement="top" title="" data-original-title="<?php echo Html::encode($user->displayName); ?>">
+						</a>
+					<?php endforeach; ?>
+				</div>			
+				<div class="clearfix"></div>
+			</div>
+			<div class="col-sm-12"><?php echo $task->title; ?></div>
 		</div>
-		<div class="task-controls pull-right">
-			<a data-toggle="collapse" href="#task-comment-<?php echo $task->id; ?>" onclick="$('#comment_humhubmodulestasksmodelsTask_<?php echo $task->id; ?>').show();return false;"
-				aria-expanded="false" aria-controls="collapseTaskComments"><i class="fa fa-comment-o">
-				</i> <?php echo $count = Comment::GetCommentCount($task->className(), $task->id); ?>
-			</a>
-		</div>
-		<div class="task-controls assigned-users pull-right" style="display: inline;">
-			<!-- Show assigned user -->
-			<?php foreach ($task->assignedUsers as $user): ?>
-				<a href="<?php echo $user->getUrl(); ?>" id="user_<?php echo $task->id; ?>"> 
-					<img src="<?php echo $user->getProfileImage()->getUrl(); ?>" class="img-rounded tt" height="24" width="24" alt="24x24" style="width: 24px; height: 24px;" data-toggle="tooltip"
-						data-placement="top" title="" data-original-title="<?php echo Html::encode($user->displayName); ?>">
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="clearfix"></div>
 	</div>
 	<div class="wall-entry collapse" id="task-comment-<?php echo $task->id; ?>">
 		<div class="wall-entry-controls">
