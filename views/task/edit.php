c8e034ac71c9479d1ac38de3cb9dc453b97c658d
andystrobel <andreas.strobel@googlemail.com>
2015-11-04 16:17:39 +0100

    Change editform to ActiveForm

--- views/task/edit.php
+++ views/task/edit.php
@@ -1,14 +1,12 @@
 <?php
-use humhub\compat\CActiveForm;
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
-use yii\jui\DatePicker;
-
 ?>
 
 <div class="modal-dialog modal-dialog-normal animated fadeIn">
     <div class="modal-content">
 
-        <?php $form = CActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(); ?>
 
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
@@ -24,60 +22,52 @@ use yii\jui\DatePicker;
 
         <div class="modal-body">
 
-
-            <div class="form-group">
-                <label
-                    for="Task_title"><?php echo Yii::t('TasksModule.views_task_edit', 'Task description'); ?></label>
-                <?php echo $form->textArea($task, 'title', array('id' => 'itemTask', 'class' => 'form-control autosize', 'rows' => '1', 'placeholder' => Yii::t('TasksModule.views_task_edit', 'What is to do?'))); ?>
-                <?php echo $form->error($task, 'title'); ?>
-            </div>
+                <?php echo $form->field($task, 'title')->textarea(['id' => 'itemTask', 'class' => 'form-control autosize', 'rows' => '1', 'placeholder' => Yii::t('TasksModule.views_task_edit', 'What is to do?')]); ?>
 
             <div class="row">
                 <div class="col-md-8">
-                    <div class="form-group">
-                        <div class="form-group">
-                            <label
-                                for="Task_preassignedUsers"><?php echo Yii::t('TasksModule.views_task_edit', 'Assign users'); ?></label>
-                            <?php echo $form->textField($task, 'assignedUserGuids', array('id' => 'assignedUserGuids', 'class' => 'form-control', 'placeholder' => Yii::t('TasksModule.views_task_edit', 'Preassign user(s) for this task.'))); ?>
-                            <?php echo $form->error($task, 'assignedUserGuids'); ?>
-                        </div>
 
-                        <?php
-                        echo humhub\modules\user\widgets\UserPicker::widget(array(
-                            'model' => $task,
-                            'inputId' => 'assignedUserGuids',
-                            'attribute' => 'assignedUserGuids',
-                            'userSearchUrl' => $this->context->contentContainer->createUrl('/space/membership/search', array('keyword' => '-keywordPlaceholder-')),
-                            'maxUsers' => 10,
-                            'placeholderText' => Yii::t('TasksModule.views_task_edit', 'Assign users to this task')
-                        ));
-                        ?>
-                    </div>
+                    <?php echo $form->field($task, 'assignedUserGuids')->textInput(['id' => 'assignedUserGuids']); ?>
+
+                    <?php
+                    // attach mention widget to it
+                    echo humhub\modules\user\widgets\UserPicker::widget(array(
+                        'model' => $task,
+                        'inputId' => 'assignedUserGuids',
+                        'attribute' => 'assignedUserGuids',
+                        'userSearchUrl' => $this->context->contentContainer->createUrl('/space/membership/search', array('keyword' => '-keywordPlaceholder-')),
+                        'maxUsers' => 10,
+                        'placeholderText' => Yii::t('TasksModule.views_task_edit', 'Assign users'),
+                    ));
+                    ?>
+
                 </div>
                 <div class="col-md-4">
 
-                    <label
-                        for="Task_deadline"><?php echo Yii::t('TasksModule.views_task_edit', 'Deadline'); ?></label>
-                    <?php
-                    echo DatePicker::widget([
-                        'model' => $task,
-                        'attribute' => 'deadline',
-                        'options' => [
-                            'class' => 'form-control',
-                            'id' => 'deadline',
-                            'placeholder' => Yii::t('TasksModule.views_task_edit', 'Deadline for this task?')
-                        ]
-                    ]);
-                    ?>
+                    <div class="form-group">
+                        <?php echo $form->field($task, 'deadline')->widget(yii\jui\DatePicker::className(), ['dateFormat' => 'short', 'clientOptions' => [], 'options' => ['class' => 'form-control', 'placeholder' => Yii::t('TasksModule.views_task_edit', 'Deadline')]]); ?>
+                    </div>
 
-                    <?php echo $form->error($task, 'deadline'); ?>
                 </div>
             </div>
             <br>
 
             <div class="row">
                 <div class="col-md-12">
-                    <?php echo Html::submitButton(Yii::t('TasksModule.views_task_edit', 'Save'), array('class' => 'btn btn-primary')); ?>
+                    <?php
+                    echo \humhub\widgets\AjaxButton::widget([
+                        'label' => Yii::t('TasksModule.views_task_edit', 'Save'),
+                        'ajaxOptions' => [
+                            'type' => 'POST',
+                            'beforeSend' => new yii\web\JsExpression('function(){ setModalLoader(); }'),
+                            'success' => new yii\web\JsExpression('function(html){ $("#globalModal").html(html); }'),
+                            'url' => $task->content->container->createUrl('/tasks/task/edit', ['id' => $task->id]),
+                        ],
+                        'htmlOptions' => [
+                            'class' => 'btn btn-primary'
+                        ]
+                    ]);
+                    ?>
 
                     <button type="button" class="btn btn-primary"
                             data-dismiss="modal"><?php echo Yii::t('TasksModule.views_task_edit', 'Cancel'); ?></button>
@@ -88,7 +78,7 @@ use yii\jui\DatePicker;
     </div>
 </div>
 
-<?php CActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 
 <script type="text/javascript">
