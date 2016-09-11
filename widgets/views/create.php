07749eb15e3e30c0b6759a9796eeb5bafc37b024
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2016-09-11 18:45:32 +0200

    Cleanup old files

--- widgets/views/create.php
+++
@@ -1,9 +0,0 @@
-<?php if ($mode == "panel") : ?>
-    <div class="panel panel-default">
-        <div class="panel-body">
-panel
-        </div>
-    </div>
-<?php elseif ($mode == "modal") : ?>
-    modal
-<?php endif; ?>
