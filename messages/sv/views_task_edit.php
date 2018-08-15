c183e2201eff9d9601a25a0ee96e625fce08cb05
HumHub Translations <info@humhub.com>
2018-08-15 11:51:51 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/views_task_edit.php
+++ messages/sv/views_task_edit.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>Create</strong> new task' => '',
-  '<strong>Edit</strong> task' => '',
-  'Assign users' => '',
+  '<strong>Create</strong> new task' => '<strong>Skapa</strong> ny uppgift',
+  '<strong>Edit</strong> task' => '<strong>Ändra</strong> uppgift',
+  'Assign users' => 'Tilldela användare',
   'Cancel' => 'Avbryt',
-  'Deadline' => '',
+  'Deadline' => 'Stopptid',
   'Save' => 'Spara',
-  'What is to do?' => '',
+  'What is to do?' => 'Vad göra?',
 );
