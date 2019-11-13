8a7ec30c9faacfb527df23748c248e57476d7eb8
HumHub Translations <info@humhub.com>
2019-11-13 15:07:14 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/models_forms_TaskFilter.php
+++ messages/it/models_forms_TaskFilter.php
@@ -1,14 +1,14 @@
 <?php
 return array (
   'Created by me' => 'Creato da me',
-  'End date' => '',
-  'Filter status' => '',
+  'End date' => 'Data fine',
+  'Filter status' => 'Stato filtro',
   'Filter tasks' => 'Filtra attività',
   'I\'m assigned' => 'Assegnato a me',
   'I\'m responsible' => 'Sono il responsabile',
   'Overdue' => 'Scaduta',
   'Spaces' => 'Space',
-  'Start date' => '',
+  'Start date' => 'Data inizio',
   'Status' => 'Stato',
   'Title' => 'Titolo',
 );
