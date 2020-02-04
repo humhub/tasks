db7cf82dbd398682f20d4cd52d2a43e4c77d60ac
HumHub Translations <info@humhub.com>
2020-02-04 10:52:40 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/config.php
+++ messages/nl/config.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    '<strong>Task</strong> module configuration' => '<strong>Taak</ strong>-module configureren',
-    'Max tasks items' => 'Maximaal aantal taken',
-    'Show snippet' => 'Fragment weergeven',
-    'Show snippet in Space' => 'Fragment in ruimte weergeven',
-    'Shows a widget with tasks on the dashboard where you are assigned/responsible.' => 'Toont een widget met aan uw toegewezen of verantwoordelijke taken op het dashboard.',
-    'Shows the widget also on the dashboard of spaces.' => 'Toont de widget ook op het dashboard van ruimtes',
-    'Sort order' => 'Sorteervolgorde',
-    'Your tasks snippet' => 'Uw taakfragment',
-    'Displays a global task menu item on the main menu.' => '',
-    'Global task menu item' => '',
-    'Menu Item sort order' => '',
-    'Show global task menu item' => '',
-];
+return array (
+  '<strong>Task</strong> module configuration' => '<strong>Taak strong&gt;-module configureren</strong>',
+  'Displays a global task menu item on the main menu.' => 'Toont een globaal taakmenu-item in het hoofdmenu.',
+  'Global task menu item' => 'Algemeen taakmenu-item',
+  'Max tasks items' => 'Maximaal aantal taken',
+  'Menu Item sort order' => 'Sorteervolgorde menu-items',
+  'Show global task menu item' => 'Toon globaal taakmenu-item',
+  'Show snippet' => 'Fragment weergeven',
+  'Show snippet in Space' => 'Fragment in ruimte weergeven',
+  'Shows a widget with tasks on the dashboard where you are assigned/responsible.' => 'Toont een widget met aan uw toegewezen of verantwoordelijke taken op het dashboard.',
+  'Shows the widget also on the dashboard of spaces.' => 'Toont de widget ook op het dashboard van ruimtes',
+  'Sort order' => 'Sorteervolgorde',
+  'Your tasks snippet' => 'Uw taakfragment',
+);
