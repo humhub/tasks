6e9d2fd0332c9d649eaaab2910fddadd75cf2882
buddh4 <mail@jharrer.de>
2020-04-06 13:15:11 +0200

    Chg: Added 1.5 defer compatibility to task Assets.php

--- assets/Assets.php
+++ assets/Assets.php
@@ -14,17 +14,31 @@ use yii\web\AssetBundle;
 class Assets extends AssetBundle
 {
 
-    public $publishOptions = [
-        'forceCopy' => false
-    ];
+    /**
+     * v1.5 compatibility defer script loading
+     *
+     * Migrate to HumHub AssetBundle once minVersion is >=1.5
+     *
+     * @var bool
+     */
+    public $defer = true;
 
+    /**
+     * @inheritDoc
+     */
     public $sourcePath = '@tasks/resources';
 
+    /**
+     * @inheritDoc
+     */
     public $css = [
         'css/task.css',
     ];
 
     // We have to use the timeentry lib for the duration since the TimePicker widget uses an older version without maxHour setting...
+    /**
+     * @inheritDoc
+     */
     public $js = [
         'js/jquery.ui.touch-punch.min.js',  // Add jQuery fix for using sortable() on mobile devices - Homepage: http://touchpunch.furf.com/
         'js/humhub.task.js',
