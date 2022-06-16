8aa9b11083e7d12de1c229479402aa4af0ec2c50
HumHub Translations <hello@humhub.com>
2022-06-16 13:18:11 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/models_forms_TaskFilter.php
+++ messages/ja/models_forms_TaskFilter.php
@@ -1,14 +1,14 @@
 <?php
 return array (
   'Created by me' => '私が作成したもの',
-  'End date' => '',
-  'Filter status' => '',
-  'Filter tasks' => '',
-  'I\'m assigned' => '',
-  'I\'m responsible' => '',
-  'Overdue' => '',
+  'End date' => '終了日',
+  'Filter status' => '状態のフィルタ',
+  'Filter tasks' => 'タスクのフィルタ',
+  'I\'m assigned' => '私がアサインされたもの',
+  'I\'m responsible' => '私に責任があるもの',
+  'Overdue' => '期限切れ',
   'Spaces' => 'スペース',
-  'Start date' => '',
+  'Start date' => '開始日',
   'Status' => 'ステータス',
   'Title' => 'タイトル',
 );
