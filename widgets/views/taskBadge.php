07204660de3014f06434a77a94f33a31a7e3ecd9
Felix Hahn <info@hahn-felix.de>
2025-07-05 21:06:51 +0200

    fix cssClass method

--- widgets/views/taskBadge.php
+++ widgets/views/taskBadge.php
@@ -17,15 +17,15 @@ use humhub\widgets\bootstrap\Badge;
 $cssClass = $right ? 'float-end' : '';
 ?>
 <?php if ($task->status == Task::STATUS_PENDING && $includePending) : ?>
-    <?= Badge::default(Yii::t('TasksModule.base', 'Pending'))->class($cssClass)->icon('info-circle') ?>
+    <?= Badge::default(Yii::t('TasksModule.base', 'Pending'))->cssClass($cssClass)->icon('info-circle') ?>
 <?php elseif ($task->status == Task::STATUS_IN_PROGRESS) : ?>
-    <?= Badge::info(Yii::t('TasksModule.base', 'In Progress'))->class($cssClass)->icon('edit') ?>
+    <?= Badge::info(Yii::t('TasksModule.base', 'In Progress'))->cssClass($cssClass)->icon('edit') ?>
 <?php elseif ($task->status == Task::STATUS_PENDING_REVIEW) : ?>
-    <?= Badge::warning(Yii::t('TasksModule.base', 'Pending Review'))->class($cssClass)->icon('eye') ?>
+    <?= Badge::warning(Yii::t('TasksModule.base', 'Pending Review'))->cssClass($cssClass)->icon('eye') ?>
 <?php elseif ($task->status == Task::STATUS_COMPLETED  && $includeCompleted) : ?>
-    <?= Badge::success(Yii::t('TasksModule.base', 'Completed'))->class($cssClass)->icon('check-square') ?>
+    <?= Badge::success(Yii::t('TasksModule.base', 'Completed'))->cssClass($cssClass)->icon('check-square') ?>
 <?php endif; ?>
 
 <?php if ($task->isOverdue()) : ?>
-    <?= Badge::danger(Yii::t('TasksModule.base', 'Overdue'))->id('taskDeadlineStatus')->class($cssClass . $right ? ' me-1"' : '')->icon('exclamation-triangle') ?>
+    <?= Badge::danger(Yii::t('TasksModule.base', 'Overdue'))->id('taskDeadlineStatus')->cssClass($cssClass . $right ? ' me-1"' : '')->icon('exclamation-triangle') ?>
 <?php endif; ?>
